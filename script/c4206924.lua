8a49232237f82a9adbf01ce01611d9702800ff19
FunnyBones777 <70409502+FunnyBones777@users.noreply.github.com>
2020-08-31 04:27:28 -0400

    6 new cards added

---
+++ script/c4206924.lua
@@ -0,0 +1,22 @@
+-- Nightmare Giant Tape
+-- Coded by FunnyBones777
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+  Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,0x420a),2)
+  --direct attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_DIRECT_ATTACK)
+	e2:SetCondition(s.dircon)
+	c:RegisterEffect(e2)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsAttribute(0xf8)
+end
+function s.dircon(e)
+	local tp=e:GetHandlerPlayer()
+	return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil)
+		and not Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil)
+end
