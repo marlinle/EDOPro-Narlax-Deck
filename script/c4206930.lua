efa6b45098e762f7df5c2735e857cbff19a3ccc3
FunnyBones777 <70409502+FunnyBones777@users.noreply.github.com>
2020-09-05 12:50:35 -0400

    2 new cards added

---
+++ script/c4206930.lua
@@ -0,0 +1,24 @@
+--Coronavirus Alpha
+--Coded by FunnyBones777
+local s,id=GetID()
+function s.initial_effect(c)
+	--pendulum summon
+	Pendulum.AddProcedure(c)
+	--Activate
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(e1)
+    --fusion
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_FZONE)
+	e2:SetProperty(EFFECT_FLAG_BOTH_SIDE)
+	
+e2:SetTarget(Fusion.SummonEffTG(nil,Card.IsAbleToRemove,nil,Fusion.BanishMaterial))
+	
+e2:SetOperation(Fusion.SummonEffOP(nil,Card.IsAbleToRemove,nil,Fusion.BanishMaterial))
+	 c:RegisterEffect(e2)
+end
