4f7b584d98f4529dc16f9dcf625133a503d92539
Rene Lino Herrera <70409502+FunnyBones777@users.noreply.github.com>
2020-12-14 23:21:58 -0500

    Edited "The Gates to the Nightmare World" card effect

--- script/c4206928.lua
+++ script/c4206928.lua
@@ -13,7 +13,7 @@ function s.initial_effect(c)
     e2:SetCode(EFFECT_UPDATE_ATTACK)
     e2:SetRange(LOCATION_FZONE)
     e2:SetTargetRange(LOCATION_MZONE,0)
-    e2:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_ZOMBIE))
+    e2:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_ZOMBIE+RACE_FIEND))
     e2:SetValue(500)
     c:RegisterEffect(e2)
     --def up
