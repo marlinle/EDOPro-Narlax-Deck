04dbae723ea7eeee1bf3bd620d8baac09577e692
Rene Lino Herrera <70409502+FunnyBones777@users.noreply.github.com>
2021-02-07 13:59:22 -0500

    "NIGHTMARE" has been changed to "Nightmare Reborn"

--- script/c4206925.lua
+++ script/c4206925.lua
@@ -1,38 +1,52 @@
--- NIGHTMARE
+-- Nightmare Reborn
 local s,id=GetID()
 function s.initial_effect(c)
-    --Special summon itself as a monster
-    local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetTarget(s.target)
-    e1:SetOperation(s.activate)
-    e1:SetHintTiming(0,TIMING_END_PHASE)
-    c:RegisterEffect(e1)
+	c:SetUniqueOnField(1,0,id)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_END_PHASE)
+	c:RegisterEffect(e1)
+	--spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0,0x21,3000,0,7,RACE_FIEND,ATTRIBUTE_DARK) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+s.listed_series={0x420}
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if not c:IsRelateToEffect(e) then return end
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
-        or not Duel.IsPlayerCanSpecialSummonMonster(tp,id,0,0x21,3000,0,7,RACE_FIEND,ATTRIBUTE_DARK) then return end
-    c:AddMonsterAttribute(TYPE_EFFECT+TYPE_TRAP)
-    Duel.SpecialSummonStep(c,0,tp,tp,true,false,POS_FACEUP)
-    c:AddMonsterAttributeComplete()
-    --Cannot be destroyed by battle
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(3000)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
-    e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetValue(1)
-    e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-    c:RegisterEffect(e1,true)
-    Duel.SpecialSummonComplete()
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x420) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(200)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
+	Duel.SpecialSummonComplete()
 end
